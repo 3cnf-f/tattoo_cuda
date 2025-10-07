0802e3493696073f9e2c258992bb9372e71424b7
3cnf-f <154888836+3cnf-f@users.noreply.github.com>
2025-10-07 23:27:49 +0200

    working easyocr of timestamp.

--- timestamp_crop_ocr.py
+++ timestamp_crop_ocr.py
@@ -5,23 +5,6 @@ import json
 
 reader = easyocr.Reader(['en'], gpu=True)  # Set gpu=False if no GPU/CUDA
 
-
-def EasyOCR_frame(inframe):
-    # Read text from image, restricted to digits and timestamp chars for optimization
-    result = reader.readtext(in_frame, allowlist='0123456789:.')  # Focus on numbers/timestamps
-    
-    # Extract texts (will be mostly/only numbers if detected)
-    texts = [r[1] for r in result]
-    
-    # Calculate approximate video timestamp
-    
-    return texts,result
-
-# Save to JSON
-
-
-
-
 folder_base = "150136"
 folder = f"../{folder_base}_4fps_sound/"
 json_file = f"{folder_base}_4fps.json"
@@ -29,6 +12,18 @@ json_file = f"{folder_base}_4fps.json"
 folder_contents= os.listdir(folder)
 folder_contents.sort()
 file = folder_contents[0]
+
+def easyocr_frame(in_frame):
+    result = reader.readtext(in_frame, allowlist='0123456789:.')  # Focus on numbers/timestamps
+    
+    
+    
+    return result
+
+
+
+
+
 def return_timestamp_cropped_img(img):
 
 # coordinates of the timestamp
@@ -42,14 +37,15 @@ def return_timestamp_cropped_img(img):
 
 for filename in folder_contents:
     filepath=folder+filename
-    print(filepath)
 
 
     img = cv2.imread(filepath)
     cropped_img = return_timestamp_cropped_img(img)
-    # cv2.imshow("cropped", cropped_img)
-    print(easyocr_frame(cropped_img))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    input("Press enter to continue")
+    cv2.imshow("cropped", cropped_img)
+    easy_ocr_result = (easyocr_frame(cropped_img))
+    easyocr_dict = {'filename':filename,'bounding_box': easy_ocr_result[0][0],'text': easy_ocr_result[0][1], 'confidence': easy_ocr_result[0][2]}
+    print(easyocr_dict)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    # input("Press enter to continue")
 # 760 100 / 1150 1060
