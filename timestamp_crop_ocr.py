549ccb2bb077432a09a98054ed88f0a595f519c3
3cnf-f <154888836+3cnf-f@users.noreply.github.com>
2025-10-07 23:48:27 +0200

    save to json

--- timestamp_crop_ocr.py
+++ timestamp_crop_ocr.py
@@ -7,7 +7,7 @@ reader = easyocr.Reader(['en'], gpu=True)  # Set gpu=False if no GPU/CUDA
 
 folder_base = "150136"
 folder = f"../{folder_base}_4fps_sound/"
-json_file = f"{folder_base}_4fps.json"
+json_file = f"{folder}{folder_base}_4fps_timstamps.json"
 
 folder_contents= os.listdir(folder)
 folder_contents.sort()
@@ -34,18 +34,21 @@ def return_timestamp_cropped_img(img):
 
     img_out = img[y:y+h,x:x+w]
     return img_out
-
+output_list = []
 for filename in folder_contents:
     filepath=folder+filename
 
 
     img = cv2.imread(filepath)
     cropped_img = return_timestamp_cropped_img(img)
-    cv2.imshow("cropped", cropped_img)
+    # cv2.imshow("cropped", cropped_img)
     easy_ocr_result = (easyocr_frame(cropped_img))
-    easyocr_dict = {'filename':filename,'bounding_box': easy_ocr_result[0][0],'text': easy_ocr_result[0][1], 'confidence': easy_ocr_result[0][2]}
-    print(easyocr_dict)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    easyocr_dict = {'filename':filename,'bounding_box': str(easy_ocr_result[0][0]),'text': str(easy_ocr_result[0][1]), 'confidence': str(easy_ocr_result[0][2])}
+    # print(easyocr_dict)
+    output_list.append(easyocr_dict)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     # input("Press enter to continue")
 # 760 100 / 1150 1060
+with open(json_file, "w", encoding="utf-8") as f:
+    json.dump(output_list, f, ensure_ascii=False, indent=4)
